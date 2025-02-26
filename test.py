b787d75c647b37f6f26b75796913998ab09b9f6c
ecorey <bengstrom32@gmail.com>
2025-02-26 11:08:47 -0500

    commit

--- test.py
+++ test.py
@@ -2,16 +2,9 @@ import torch
 from torch import nn
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_circles
 
 
 
-n_samples = 1000
-
-X, y = make_circles(n_samples, noise=0.03, random_state=42)
-
-plt.scatter(x=X[:, 0], y=X[:, 1], c=y, cmap=plt.cm.RdYlBu)
-plt.show()
 
 
 
