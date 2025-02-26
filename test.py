df7184dfcfdbc26e51f94cf5f0d56d9c4e8690c8
ecorey <bengstrom32@gmail.com>
2025-02-26 12:07:22 -0500

    commit

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -23,3 +24,26 @@ y = df['P90']
 print(X.head())
 print("-------")
 print(y.head())
+
+
+# encode station as it is categorical
+label_encoder = LabelEncoder()
+X['Station'] = label_encoder.fit_transform(X['Station'])
+
+print("encoded station:")
+print(df['Station'].head())
+
+
+
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(X)
+
+
+# split data into train and test
+X_train, X_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
+
+
+print("Training Features Shape:", X_train.shape)
+print("Testing Features Shape:", X_test.shape)
+print("Training Target Shape:", y_train.shape)
+print("Testing Target Shape:", y_test.shape)
